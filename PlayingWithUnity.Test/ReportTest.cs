e69569910857fa8d733245bbd5defc24253eacf5
Ken Wilcox <wilcoxk@gmail.com>
2015-03-02 13:29:52 -0700

    removed unused usings

--- PlayingWithUnity.Test/ReportTest.cs
+++ PlayingWithUnity.Test/ReportTest.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using PlayingWithUnity.Interface;
-using PlayingWithUnity.Implementation;
+﻿using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using PlayingWithUnity.Implementation;
+using PlayingWithUnity.Interface;
 
 namespace PlayingWithUnity.Test
 {
@@ -18,14 +17,14 @@ namespace PlayingWithUnity.Test
 
       var testData = new List<string> {"This", "Is", "Another", "Test"};
 
-      fetcher.Setup(s => s.Fetch()).Returns(() => { return testData; });
+      fetcher.Setup(s => s.Fetch()).Returns(() => testData ?? null);
       transformer.Setup(s => s.Transform(It.IsAny <IEnumerable<string>>())).Returns(It.IsAny<string>());
 
       var report = new Report(fetcher.Object, transformer.Object);
       report.Execute();
 
       fetcher.Verify(v => v.Fetch(), Times.Once);
-      transformer.Verify(v => v.Transform(It.Is<IEnumerable<string>>(i => i == testData)), Times.Once);
+      transformer.Verify(v => v.Transform(It.Is<IEnumerable<string>>(i => i.Equals(testData))), Times.Once);
     }
   }
 }
